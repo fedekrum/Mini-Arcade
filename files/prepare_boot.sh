04ecb677d7ddfb4b6097be9d04416efbacc81dd1
Federico Krum <federico@krum.com.ar>
2022-11-19 21:52:59 +0100

    cc

---
+++ files/prepare_boot.sh
@@ -0,0 +1,10 @@
+# RUN THIS IN MY MAC AFTER RETROPIE INSTALLATION
+
+# Create a file called /boot/wpa_supplicant.conf using the following template. 
+# This will be moved at boot to the /etc/wpa_supplicant directory.
+
+cd /Volumes/boot
+touch /Volumes/boot/ssh.txt
+wget 
+
+nano /Volumes/boot/wpa_supplicant.conf
