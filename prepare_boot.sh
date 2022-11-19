016760ee260552e9138ec90842b2f5dccdd6dff4
Federico Krum <federico@krum.com.ar>
2022-11-19 22:42:59 +0100

    vhvnh

--- prepare_boot.sh
+++ prepare_boot.sh
@@ -1,9 +1,5 @@
 # RUN THIS IN MY MAC AFTER RETROPIE INSTALLATION
 
-# Create a file called /boot/wpa_supplicant.conf using the following template. 
-# This will be moved at boot to the /etc/wpa_supplicant directory.
-
-
 if [ ! -f /Volumes/boot/ssh.txt ]
 then
     touch /Volumes/boot/ssh.txt
@@ -19,6 +15,12 @@ then
     touch /Volumes/boot/ssh.txt
 fi
 
+# Replaces the original 
 wget https://raw.githubusercontent.com/fedekrum/Mini-Arcade/main/files/boot/config.txt -O /Volumes/boot/config.txt
+
+# Creates a file called /boot/wpa_supplicant.conf using the following template. 
+# This will be moved at boot to the /etc/wpa_supplicant directory.
 wget https://raw.githubusercontent.com/fedekrum/Mini-Arcade/main/files/boot/wpa_supplicant.conf -O /Volumes/boot/wpa_supplicant.conf
+
+# Edit your wifi settings
 nano /Volumes/boot/wpa_supplicant.conf
