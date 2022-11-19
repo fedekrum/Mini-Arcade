41286d96fb821e1b9ad376f6e466f784ba43237b
Federico Krum <federico@krum.com.ar>
2022-11-19 22:19:17 +0100

    cc

--- prepare_boot.sh
+++ prepare_boot.sh
@@ -3,8 +3,22 @@
 # Create a file called /boot/wpa_supplicant.conf using the following template. 
 # This will be moved at boot to the /etc/wpa_supplicant directory.
 
-touch /Volumes/boot/ssh.txt
-mv /Volumes/boot/config.txt /Volumes/boot/config.txt.orig
+
+if [ ! -f /Volumes/boot/ssh.txt ]
+then
+    touch /Volumes/boot/ssh.txt
+fi
+
+if [ ! -f /Volumes/boot/config.txt.orig ]
+then
+    mv /Volumes/boot/config.txt /Volumes/boot/config.txt.orig
+fi
+
+if [ ! -f /Volumes/boot/ssh.txt ]
+then
+    touch /Volumes/boot/ssh.txt
+fi
+
 wget https://raw.githubusercontent.com/fedekrum/Mini-Arcade/main/files/boot/config.txt -O /Volumes/boot/config.txt
 wget https://raw.githubusercontent.com/fedekrum/Mini-Arcade/main/files/boot/wpa_supplicant.conf -O /Volumes/boot/wpa_supplicant.conf
 nano /Volumes/boot/wpa_supplicant.conf
